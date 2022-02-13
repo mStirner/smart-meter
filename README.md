743c9ab25f6508433452b4e3ea8033b7b62ed859
mStirner <marc.stirner@hotmail.de>
2022-02-13 12:41:07 +0100

    WebSocket server added

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ I use this for a [Eastron SDM72DM-V2](https://stromzähler.eu/stromzaehler/drehs
 
 I tried various modbus packages from npm, but no ones workerd. To get what i want, i used the "EEM Meter" tool from the manufacturer and reverse enginiered the payload that is send over the USB/RS485 adapter.
 
+![EEM Meter](./docs/153709176-e3e34143-86ce-4230-8c22-52f4045b5828.PNG)
+
 
 ## Example Output
 ```
@@ -26,6 +28,10 @@ Total active Energy: 244.75kWh
 This is currently just a output to the terminal/stdout.<br>
 Later i want to send the values over a websocket connection to my [SmartHome solution](https://github.com/OpenHausIO)
 
+> My Power/Energy is negative because i switched input/output from the mains (Hauptverteilung).
+> Otherwise i had to mount the meter upside down, which is uggly.
+> The meter can be used for import & export (Solar Panel) measurements.
+
 ## Installation
 Just clone the repo, cd into the directory and do `npm install`.
 After the successful installation of the npm packages, execute `node index.js` to read/write data.
@@ -34,16 +40,23 @@ After the successful installation of the npm packages, execute `node index.js` t
 
 | Environment variable | Default value  | Description                                                         |
 | -------------------- | -------------- | ------------------------------------------------------------------- |
+| NODE_ENV             | `production`   | Set to `development` to print debug messages                        |
 | SERIAL_DEVICE        | `/dev/ttyUSB0` | USB/RS485 Serial Adapter                                            |
 | SERIAL_BAUDRATE      | `9600`         | Baudrate for serial communication                                   |
 | SERIAL_PARITY        | `none`         | Parity for serial communication                                     |
 | SERIAL_STOPBITS      | `1`            | Stop bits for serial communication                                  |
 | SERIAL_DATABITS      | `8`            | Data bits for serial communication                                  |
 | QUERY_INTERVAL       | `5000`         | Time in ms how often the input registers should be quiered/refreshd |
+| HTTP_ENABLED         | `true`         | Start a HTTP (WebSocket) Server if `true`                           |
+| HTTP_ADDRESS         | `0.0.0.0`      | HTTP Server listening interface                                     |
+| HTTP_PORT            | `8080`         | Websocket Server port                                               |
+| PRINT_STDOUT         | `true`         | Print register values to stdout                                     |
+| DEBUG                | `smart-meter`  | https://www.npmjs.com/package/debug                                 |
+| SMART_METER_ID       | `1`            | ModBus slave ID                                                     |
 
 ## Miscellaneous
 Is this a full modbus implemantion?
 >This is not a full modbus implementation. I only managed to read the input registers, writing is currently not supported.
 
-The input registers are not complete, in the [datasheet](https://stromzähler.eu/media/pdf/93/17/d7/SDM72DM-V2.pdf) are more listet than quired here.
+The input registers are not complete, in the [datasheet](./docs/SDM72DM-V2.pdf) are more listet than quired here.
 > I dont need all registers, in the file `register.js` you can add or remove the one you need.
--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const Serialport = require('serialport');
-const ByteLength = require('@serialport/parser-byte-length');
-const checksum = require("./checksum.js");
-
-// holding registers
-const registers = new Set();
-require("./registers.js")(registers);
-
-
-
 process.env = Object.assign({
+    NODE_ENV: "production",
     SERIAL_DEVICE: "/dev/ttyUSB0",
     SERIAL_BAUDRATE: "9600",
     SERIAL_PARITY: "none",
     SERIAL_STOPBITS: "1",
     SERIAL_DATABITS: "8",
-    QUERY_INTERVAL: "5000"
+    QUERY_INTERVAL: "5000",
+    HTTP_ENABLED: "true",
+    HTTP_ADDRESS: "0.0.0.0",
+    HTTP_PORT: "8080",
+    PRINT_STDOUT: "true",
+    DEBUG: "smart-meter",
+    SMART_METER_ID: "1"
 }, process.env);
+
+
+const http = require("http");
+const Serialport = require("serialport");
+const ByteLength = require("@serialport/parser-byte-length");
+const checksum = require("./checksum.js");
+const WebSocket = require("ws");
+
+
+var debug = () => { };
+
+if (process.env.NODE_ENV === "development") {
+    debug = require("debug")("smart-meter");
+}
+
+
+// holding registers
+const registers = new Set();
+require("./registers.js")(registers);
 
 
 const port = new Serialport(process.env.SERIAL_DEVICE, {
@@ -25,27 +41,48 @@ const port = new Serialport(process.env.SERIAL_DEVICE, {
     dataBits: Number(process.env.SERIAL_DATABITS)
 });
 
+const server = http.createServer();
 
+const wss = new WebSocket.WebSocketServer({
+    server
+});
+
+if (process.env.HTTP_ENABLED === "true") {
+    server.listen(Number(process.env.HTTP_PORT), process.env.HTTP_ADDRESS, (err) => {
+        debug(err || `HTTP Server listneing on http://${process.env.HTTP_ADDRESS}:${process.env.HTTP_PORT}`);
+    });
+}
+
+wss.on("connection", () => {
+    debug("WebSocket client connected");
+});
 
 
 port.open(() => {
 
-    console.log("Serial port open")
+    // feedback
+    debug("Serial port open");
 
-
+    // parse response
     const parser = port.pipe(new ByteLength({
         length: 9
     }));
 
     const query = () => {
 
+        // feedback
+        debug("Query function called");
+
+        // wrap all queries (reading input register) as promise
+        // build out of the proimse array a call stack
+        // that execute one reading after the other
         let wrapper = Array.from(registers).map((register) => {
             return () => new Promise((resolve, reject) => {
 
                 let { address } = register;
 
                 let message = Buffer.concat([
-                    Buffer.from([0x01, 0x04]),  // header: [0] slave address; [1] function code
+                    Buffer.from([Number(process.env.SMART_METER_ID), 0x04]),  // header: [0] slave address; [1] function code
                     address,                    // register address
                     Buffer.from([0x00, 0x02])   // "footer"
                 ]);
@@ -70,7 +107,7 @@ port.open(() => {
                 }
 
                 // listen for response
-                parser.once('data', incoming);
+                parser.once("data", incoming);
 
                 // send "request"
                 port.write(payload);
@@ -79,21 +116,37 @@ port.open(() => {
         });
 
 
-
+        // build out of the promises array a chain
+        // "call stack" for query each after the other
         wrapper.reduce((cur, prev) => {
             return cur.then(prev);
         }, Promise.resolve()).then(() => {
 
-            //console.log("Registers queried", registers)
+            let register = Array.from(registers);
+            let json = JSON.stringify(register);
 
-            process.stdout.write("\033c");
-
-            console.log("Updated: %d", Date.now());
-
-            Array.from(registers).forEach(({ description, value, unit }) => {
-                console.log(`${description}: ${value}${unit}`)
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(json);
+                }
             });
 
+
+            if (process.env.PRINT_STDOUT === "true") {
+
+                process.stdout.write("\033c");
+
+                console.log("Updated: %d", Date.now());
+
+                register.forEach(({ description, value, unit }) => {
+                    console.log(`${description}: ${value}${unit}`);
+                });
+
+            }
+
+            debug("Queryied", json);
+
+            // wait x of ms before calling ourself
             setTimeout(query, Number(process.env.QUERY_INTERVAL));
 
         });
@@ -101,64 +154,7 @@ port.open(() => {
 
     };
 
+    // start query loop
     query();
 
-
 });
-
-
-
-
-
-
-/*
-port.open(() => {
-
-    console.log("Serial port open")
-
-    const parser = port.pipe(new ByteLength({
-        length: 9
-    }));
-
-    setInterval(() => {
-
-        console.log("Query registers")
-
-        registers.forEach(({ address, description, value, unit }) => {
-
-            let message = Buffer.concat([
-                Buffer.from([0x01, 0x04]),  // header: [0] slave address; [1] function code
-                address,                    // register address
-                Buffer.from([0x00, 0x02])   // "footer"        
-            ]);
-
-            let payload = Buffer.concat([
-                message,
-                checksum(message)
-            ]);
-
-            console.log("Send", payload)
-
-            port.write(payload);
-
-            parser.once('data', (data) => {
-
-                let bytes = data.slice(3, -2);
-                let val = bytes.readFloatBE(0).toFixed(2);
-
-                // save value returnd
-                value = val;
-
-                // feedback
-                console.log(`${description}: ${val} ${unit}`);
-
-            });
-
-        });
-
-        console.log(registers);
-
-    }, 5000);
-
-});
-*/
